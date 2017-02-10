83662106a6cfb8928a28e78ca9268191ab49647d
Max Z <max.zlotskiy@gmail.com>
2017-02-10 06:38:15 -0500

    Started writing placeQueen

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -13,6 +13,16 @@ public class QueenBoard {
     public String getCount() {return "";}
     public String toString() {return "";}
 
+    private void placeQueen(int row, int col) {
+	board[row][col] = 1;
+	for(int c = 0; c < board.length; c++) {
+	    if(c != col) {board[row][c] = -1;}
+	}
+	for(int r = 0; r < board.length; r++) {
+	    if(r != row) {board[r][col] = -1;}
+	}
+    }
+
     private void clear() {
 	for(int i = 0; i < board.length; i++) {
 	    for(int j = 0; j < board[i].length; j++) {
