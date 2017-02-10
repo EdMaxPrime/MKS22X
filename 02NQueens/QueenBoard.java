c516fb26232b0ef01c253a72b19d722d48d52e46
Max Z <max.zlotskiy@gmail.com>
2017-02-10 06:26:25 -0500

    Skeleton

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,9 @@
+public class QueenBoard {
+    private int[][] board;
+    private int solutionCount;
+
+    public void solve() {}
+    public void countSolutions() {}
+    public String getCount() {return "";}
+    public String toString() {return "";}
+}
