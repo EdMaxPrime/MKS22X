887b299b3add161b2b6ed206db937b0138886ac0
Max Z <max.zlotskiy@gmail.com>
2017-02-10 06:34:07 -0500

    Added main, constructor, and clear

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -2,8 +2,26 @@ public class QueenBoard {
     private int[][] board;
     private int solutionCount;
 
+    public QueenBoard(int size) {
+	board = new int[size][size];
+	clear();
+	solutionCount = 0;
+    }
+
     public void solve() {}
     public void countSolutions() {}
     public String getCount() {return "";}
     public String toString() {return "";}
+
+    private void clear() {
+	for(int i = 0; i < board.length; i++) {
+	    for(int j = 0; j < board[i].length; j++) {
+		board[i][j] = 0;
+	    }
+	}
+    }
+
+    public static void main(String[] args) {
+	QueenBoard a = new QueenBoard(4);
+    }
 }
