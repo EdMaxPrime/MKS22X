ea98b8817e09b4a77be620949f029e90e54ae289
Max Z <max.zlotskiy@gmail.com>
2017-02-15 06:10:25 -0500

    Basic structure, compiles

---
+++ 03KnightsTour/KnightBoard.java
@@ -0,0 +1,15 @@
+public class KnightBoard {
+    public KnightBoard(int startingRows, int startingCols) {
+	
+    }
+
+    public void solve() {}
+
+    public String toString() {
+	return "";
+    }
+
+    public static void main(String[] args) {
+	//
+    }
+}
