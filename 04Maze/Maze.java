794379fee1ad0b83e7820b0e38c6a753d55a62d5
Max Z <max.zlotskiy@gmail.com>
2017-02-28 15:19:23 -0500

    wrote skeleton of maze, compiles

---
+++ 04Maze/Maze.java
@@ -0,0 +1,14 @@
+public class Maze {
+    private char[][] maze;
+    private boolean animate;
+
+    public Maze(String filename) {}
+
+    public void setAnimate(boolean b) {}
+
+    public void clearTerminal() {}
+
+    public boolean solve() {return false;}
+
+    private boolean solve(int row, int col) {return false;}
+}
