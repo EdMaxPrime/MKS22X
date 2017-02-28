967aaebba1fd8a24fc5d2f2be3706df1999198f1
Max Z <max.zlotskiy@gmail.com>
2017-02-28 15:35:13 -0500

    wrote part of the constructor, compiles

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,8 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Maze {
     private char[][] maze;
     private boolean animate;
 
-    public Maze(String filename) {}
+    public Maze(String filename) {
+	try {
+	    Scanner in = new Scanner(new File(filename));
+	    while(in.hasNextLine()) {
+		char[] line = in.nextLine().toCharArray();
+	    }
+	} catch(FileNotFoundException e) {
+	    System.out.println('"'+filename+"\" not found");
+	    System.exit(0);
+	}
+    }
 
     public void setAnimate(boolean b) {}
 
@@ -11,4 +25,8 @@ public class Maze {
     public boolean solve() {return false;}
 
     private boolean solve(int row, int col) {return false;}
+
+    public static void main(String[] args) {
+	Maze a = new Maze("data1.txt");
+    }
 }
