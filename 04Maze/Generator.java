41d2653ab4b845d80faeb62e35e065c447fe5971
Max Z <max.zlotskiy@gmail.com>
2017-03-04 13:33:13 -0500

    created skeleton for generator

---
+++ 04Maze/Generator.java
@@ -0,0 +1,13 @@
+public class Generator {
+    public Generator(int width, int height) {}
+
+    private class Cell {
+	private boolean north, south, east, west;
+	private int id;
+	public Cell(int _id) {}
+    }
+
+    private class Set {
+	public Set(int id) {}
+    }
+}
