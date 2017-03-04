44175b225c0e0f29d3d1c99f333352cf0ddd3fa2
Max Z <max.zlotskiy@gmail.com>
2017-03-04 13:45:26 -0500

    fleshed out the Set class

--- 04Maze/Generator.java
+++ 04Maze/Generator.java
@@ -3,11 +3,35 @@ public class Generator {
 
     private class Cell {
 	private boolean north, south, east, west;
-	private int id;
-	public Cell(int _id) {}
+	private Set set;
+	public Cell(Set s) {
+	    set = s;
+	    north = false;
+	    south = false;
+	    east = false;
+	    west = false;
+	}
     }
 
     private class Set {
-	public Set(int id) {}
+	private int id;
+	private Set parent;
+	public Set(int _id) {
+	    id = _id;
+	    parent = null;
+	}
+	public void joinFamily(Set family) {
+	    parent = family;
+	}
+	public boolean independent() {
+	    return parent == null;
+	}
+	public Set getFamily() {
+	    if(independent()) return this;
+	    else              return parent.getFamily();
+	}
+	public boolean equals(Set other) {
+	    return other.id == this.id;
+	}
     }
 }
