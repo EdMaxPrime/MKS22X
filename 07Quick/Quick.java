19f709daa4a0bf0315964139c76fbfc136e18f6b
Max Z <max.zlotskiy@gmail.com>
2017-03-15 15:17:26 -0400

    added printing methods

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -6,7 +6,7 @@ public class Quick {
     }
 
     public static int part(int[] data, int start, int end) {
-	int pivot = data[(new Random()).nextInt(start, end+1)];
+	int pivot = data[(new Random()).nextInt(end - start + 1) + start];
 	int rightmost = end, leftmost = start;
 	for(int i = start; i <= end; i++) {
 	    if(data[i] < pivot) {
@@ -24,5 +24,21 @@ public class Quick {
 	    }
 	}
 	return leftmost; //index of the pivot
+    }
+
+    public static String arr2str(int[] arr) {
+	String str = "[";
+	for(int i : arr) {str += i + ", ";}
+	if(str.endsWith(", ")) {str = str.substring(0, str.length()-2);}
+	str += "]";
+	return str;
+    }
+
+    public static void printArr(int[] arr) {
+	System.out.println(arr2str(arr));
+    }
+
+    public static void printArr(String message, int[] arr) {
+	System.out.println(message.replaceFirst("%a", arr2str(arr)));
     }
 }
