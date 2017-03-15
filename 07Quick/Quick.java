2b1bf6f24d6988a9ca4ccfbb98fd270dabc00fc7
Max Z <max.zlotskiy@gmail.com>
2017-03-15 15:10:45 -0400

    wrote first version of the partition method

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,9 +1,28 @@
+import java.util.Random;
+
 public class Quick {
     public static void main(String[] args) {
 	//
     }
 
     public static int part(int[] data, int start, int end) {
-	return 0;
+	int pivot = data[(new Random()).nextInt(start, end+1)];
+	int rightmost = end, leftmost = start;
+	for(int i = start; i <= end; i++) {
+	    if(data[i] < pivot) {
+		//swap with leftmost
+		int temp = data[i];
+		data[i] = data[leftmost];
+		data[leftmost] = temp;
+		leftmost++;
+	    } else {
+		//swap with rightmost
+		int temp = data[i];
+		data[i] = data[rightmost];
+		data[rightmost] = data[i];
+		rightmost--;
+	    }
+	}
+	return leftmost; //index of the pivot
     }
 }
