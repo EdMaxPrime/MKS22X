7726b8b64399af9311179df6bfcf3bdeb2e972d0
Max Z <max.zlotskiy@gmail.com>
2017-03-16 15:34:21 -0400

    new algorithm work when pivot is not the greatest element

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Quick {
     public static void main(String[] args) {
-	int[] a = {0, 3, 5, 7, 6};
+	int[] a = {0, 10, 20, 30, 40};
 	printArr("Start: %a", a);
 	System.out.printf("Returned %d%n",  part(a, 0, a.length-1));
 	printArr("And now: %a", a);
@@ -10,7 +10,28 @@ public class Quick {
 
     public static int part(int[] data, int start, int end) {
 	int pivotIndex = (new Random()).nextInt(end - start + 1) + start;
-	
+	swap(data, end, pivotIndex);
+	int pivot = data[end], wall = end - 1;
+	for(int i = 0; i < wall; ) {
+	    if(data[i] >= pivot) {
+		swap(data, i, wall);
+		wall--;
+	    } else {
+		i++;
+	    }
+	}
+	System.out.println("Sorting around " + pivot);
+	swap(data, wall, end);
+	return wall;
+    }
+
+    /**
+       Yuck
+     */
+    private static void swap(int[] array, int indexA, int indexB) {
+	int temp = array[indexA];
+	array[indexA] = array[indexB];
+	array[indexB] = temp;
     }
 
     public static int partFake(int[] data, int start, int end) {
