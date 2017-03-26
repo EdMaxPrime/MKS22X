668cc1d2019585a0e20a8fd9a6500cb0ae6d65a4
Max Z <max.zlotskiy@gmail.com>
2017-03-25 20:39:23 -0400

    wrote the plan

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -4,6 +4,9 @@ public class Merge {
     }
 
     public static void mergesort(int[] array) {
-	//
+	//base case: length of 1, return array
+	//sort left half
+	//sort right half
+	//merge the two halves
     }
 }
