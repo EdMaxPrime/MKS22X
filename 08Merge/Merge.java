2017b17c9d53c691e3c706e6ea516f41ca370154
Max Z <max.zlotskiy@gmail.com>
2017-03-25 20:53:02 -0400

    imported over array utilites

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Merge {
     public static void main(String[] args) {
 	//tests
@@ -38,5 +40,108 @@ public class Merge {
 		start++;
 	    }
 	}
+    }
+
+    /**
+       Yuck
+     */
+    private static void swap(int[] array, int indexA, int indexB) {
+	int temp = array[indexA];
+	array[indexA] = array[indexB];
+	array[indexB] = temp;
+    }
+
+    /**
+       Makes an array of random positive integers less than 100
+       @param len  if len is &lt; 0, array will be of random length,
+                   otherwise the array will be of len length
+       @return     the array
+     */
+    public static int[] randomArray(int len) {
+	Random rng = new Random();
+	if(len < 0) len = (int)(Math.abs(rng.nextInt()));
+	int[] array = new int[len];
+	while(len > 0) {
+	    array[len - 1] = rng.nextInt(100);
+	    len--;
+	}
+	return array;
+    }
+
+    /**
+       Makes an array of random positive integers less than 100
+       with some of them duplicated.
+       @param len  if len is &lt; 0, array will be of random length,
+                   otherwise the array will be of len length
+       @return     the array
+     */
+    public static int[] randomArrayDuplicates(int len) {
+	Random rng = new Random();
+	if(len < 0) len = (int)(Math.abs(rng.nextInt()));
+	int[] array = new int[len];
+	int repeat = 0;
+	while(len > 0) {
+	    if(repeat == 0) {
+		repeat = 1 + rng.nextInt(5);
+		array[len - 1] = rng.nextInt(100);
+	    } else {
+		array[len - 1] = array[len];
+	    }
+	    repeat--;
+	    len--;
+	}
+	shuffle(array);
+	return array;
+    }
+
+    /**
+       Shuffles an array by swapping elements randomly
+       @param array  the array to be randomized
+     */
+    public static void shuffle(int[] array) {
+	Random rng = new Random();
+	for(int i = 0; i < array.length; i++) {
+	    swap(array, i, rng.nextInt(array.length));
+	}
+    }
+
+    /**
+       Serves as a toString for integer arrays
+       @param arr  the array
+       @return     a string in the format "[a, b, c, ... z]"
+     */
+    public static String arr2str(int[] arr) {
+	String str = "[";
+	for(int i : arr) {str += i + ", ";}
+	if(str.endsWith(", ")) {str = str.substring(0, str.length()-2);}
+	str += "]";
+	return str;
+    }
+
+    public static void printArr(int[] arr) {
+	System.out.println(arr2str(arr));
+    }
+
+    /**
+       Prints a formatted string where the only supported
+       type is and integer array represented as %a.
+       @param message  a string to be printed, should have %a
+       @param arr      the array to replace %a with
+     */
+    public static void printArr(String message, int[] arr) {
+	System.out.println(message.replaceFirst("%a", arr2str(arr)));
+    }
+
+    /**
+       Makes a copy of an array. No references remain
+       @param array  the array to copy
+       @return       a copy of that array
+     */
+    public static int[] copy(int[] array) {
+	int[] copy = new int[array.length];
+	for(int i = 0; i < array.length; i++) {
+	    copy[i] = array[i];
+	}
+	return copy;
     }
 }
