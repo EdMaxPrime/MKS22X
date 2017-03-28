abb03355715420c9c1250da92c449ca62577cdd8
Max Z <max.zlotskiy@gmail.com>
2017-03-27 23:20:33 -0400

    getNode is more efficient, it can iterate backwards too now

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -45,9 +45,17 @@ public class MyLinkedList {
     private LNode getNode(int index) {
 	if(index < 0) index += size; //backwards indexing
 	LNode current = start;
-	while(index > 0) {
-	    current = current.next;
-	    index--;
+	if(size == 1 || index < size/2) {
+	    while(index > 0) {
+		current = current.next;
+		index--;
+	    }
+	} else {
+	    current = last;
+	    while(index < size) {
+		current = current.prev;
+		index++;
+	    }
 	}
 	return current;
     }
