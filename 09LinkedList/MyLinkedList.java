586d3ef64f16ab8b508f5c2ecd402b575ff58043
Max Z <max.zlotskiy@gmail.com>
2017-03-27 22:33:12 -0400

    wrote default constructor

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,8 +1,17 @@
 public class MyLinkedList {
+    private LNode start;
+    private int size;
 
+    public MyLinkedList() {
+	start = null;
+	size = 0;
+    }
+    
     private class LNode {
 	
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+	MyLinkedList a = new MyLinkedList();
+    }
 }
