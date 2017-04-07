32ddc7ba0ff635f6888ff5d8beccd139e8c2dbd8
Max Z <max.zlotskiy@gmail.com>
2017-04-07 15:13:30 -0400

    wrote size

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -8,4 +8,12 @@ public class MyDeque {
 	front = content.length/2;
 	back = front;
     }
+    public int size() {
+	if(front < back) return back - front;
+	return content.length - Math.abs(front - back);
+    }
+
+    public static void main(String[] args) {
+	//tests
+    }
 }
