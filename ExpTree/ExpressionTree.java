b11e8c93bf580d2f6cafad139c5d2b19ed9162b8
Max Z <max.zlotskiy@gmail.com>
2017-04-27 15:06:18 -0400

    base of expression tree class

---
+++ ExpTree/ExpressionTree.java
@@ -0,0 +1,48 @@
+public class ExpressionTree {
+    private char op;
+    private double value;
+    private ExpressionTree left,right;
+  
+    /*TreeNodes are immutable, so no issues with linking them across multiple
+    *  expressions. The can be constructed with a value, or operator and 2
+    * sub-ExpressionTrees*/
+    public ExpressionTree(double value){
+	this.value = value;
+	op = '~';
+    }
+    public ExpressionTree(char op,ExpressionTree l, ExpressionTree r){
+	this.op = op;
+	left = l;
+	right = r;
+    }
+  
+  
+  
+    public char getOp(){
+	return op;
+    }
+  
+    /* accessor method for Value, precondition is that isValue() is true.*/
+    private double getValue(){
+	return value;
+    }
+    /* accessor method for left, precondition is that isOp() is true.*/
+    private ExpressionTree getLeft(){
+	return left;
+    }
+    /* accessor method for right, precondition is that isOp() is true.*/
+    private ExpressionTree getRight(){
+	return right;
+    }
+  
+    private boolean isOp(){
+	return hasChildren();
+    }
+    private boolean isValue(){
+	return !hasChildren();
+    }
+  
+    private boolean hasChildren(){
+	return left != null && right != null;
+    }
+}
