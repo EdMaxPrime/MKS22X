e5450bf316ee104dfe3aacd7c9dbf8b68576f9d3
Max Z <max.zlotskiy@gmail.com>
2017-05-04 14:58:28 -0400

    using string instead of int

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,14 +1,18 @@
 public class MyHeap {
-    int[] contents;
+    String[] contents;
     int size, dir;
 
     public MyHeap(boolean maxMin) {
-	contents = new int[10];
+	contents = new String[16];
 	size = 0;
 	dir = maxMin? 1 : -1;
     }
 
     public MyHeap() {
 	this(true);
+    }
+
+    public void add(String str) {
+	makeSpace();
     }
 }
