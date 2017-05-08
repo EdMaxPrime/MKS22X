dfa16f604a1dad2e1a65d7a6af5e8c932e2f71ab
Max Z <max.zlotskiy@gmail.com>
2017-05-08 15:24:18 -0400

    started median class

---
+++ 14Median/RunningMedian.java
@@ -0,0 +1,11 @@
+public class RunningMedian {
+    private MyHeap less, more;
+    public RunningMedian() {
+	less = new MyHeap();
+	more = new MyHeap(false);
+    }
+
+    public void add(int number) {}
+
+    public double getMedian() {}
+}
