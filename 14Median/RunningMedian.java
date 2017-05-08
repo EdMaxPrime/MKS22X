c0a7c4819b4a16247533ffdfe2e293fe73789c99
Max Z <max.zlotskiy@gmail.com>
2017-05-08 15:29:56 -0400

    started writing getMedian

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -1,11 +1,26 @@
 public class RunningMedian {
     private MyHeap less, more;
+    private int l1, l2;
     public RunningMedian() {
 	less = new MyHeap();
 	more = new MyHeap(false);
+	l1 = 0;
+	l2 = 0;
     }
 
-    public void add(int number) {}
+    public void add(int number) {
+	//
+    }
 
-    public double getMedian() {}
+    public double getMedian() {
+	if(l1 == l2) {
+	    if(l1 == 0) return 0; //error?
+	    return (getLess()+getMore()) / 2.0;
+	}
+	return 0; //other cases here
+    }
+
+    private double getLess() {}
+
+    private double getMore() {}
 }
