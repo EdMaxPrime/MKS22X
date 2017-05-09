9cbe7c8a0aca819594cb8b059386bca7f8300053
Max Z <max.zlotskiy@gmail.com>
2017-05-09 19:16:19 -0400

    wrote add()

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -8,8 +8,38 @@ public class RunningMedian {
 	l2 = 0;
     }
 
+    /**
+       Adds a number to the list of numbers used to calculate
+       the median. If the two heaps are of equal length, then
+       the number is added depending on its relation to the
+       median(LESS or MORE). If one heap is bigger but the
+       number belongs in the other, then it is added as so.
+       If one heap is bigger *and* the number belongs in that
+       heap, then the heaps are balanced out before the new
+       number is added.
+     */
     public void add(int number) {
-	//
+	if(number < getMedian()) {
+	    if(l1 <= l2) {
+		less.add(number);
+		l2++;
+	    } else {
+		more.add(less.remove());
+		l2++;
+		l1--;
+		add(number);
+	    }
+	} else {
+	    if(l2 <= l1) {
+		more.add(number);
+		l2++;
+	    } else {
+		less.add(more.remove());
+		l1++;
+		l2--;
+		add(number);
+	    }
+	}
     }
 
     public double getMedian() {
