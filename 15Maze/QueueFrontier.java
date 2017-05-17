139fdb5f199dd19a82597297e0fb7740353d43ab
Max Z <max.zlotskiy@gmail.com>
2017-05-17 15:13:10 -0400

    wrote queue frontier

---
+++ 15Maze/QueueFrontier.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class QueueFrontier implements Frontier {
+    private LinkedList<Node> frontier;
+
+    public QueueFrontier() {
+	frontier = new LinkedList<Node>();
+    }
+
+    public Node next() {
+	return frontier.removeFirst();
+    }
+
+    public boolean hasNext() {
+	return frontier.size() == 0;
+    }
+
+    public void add(Node n) {
+	frontier.addLast(n);
+    }
+}
