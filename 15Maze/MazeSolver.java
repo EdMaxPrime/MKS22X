85fa621ecc76245f738f949c75c7dd936064c6cf
Max Z <max.zlotskiy@gmail.com>
2017-05-18 15:30:12 -0400

    started solve algorithm

--- 15Maze/MazeSolver.java
+++ 15Maze/MazeSolver.java
@@ -19,5 +19,29 @@ public class MazeSolver {
 	    f = new StackFrontier();
 	}
 	f.add(board.getStart());
+	while(f.hasNext()) {
+	    Node here = f.next();
+	    char type = board.get(here.loc.row(), here.loc.col());
+	    if(type == 'E') {
+		return; //we finished
+	    }
+	    Node[] more = getNeighbors(here);
+	    for(int i = 0; i < 4; i++) {
+		if(more[i] != null)
+		    f.add(more[i]); //also mark as ?
+	    }
+	    board.set(here.loc.row(), here.loc.col(), '.'); //visited
+	}
+    }
+
+    public Node[] getNeighbors(Node center) {
+	Node[] neighbors = new Node[4];
+	int howMany = 0;
+	int r = center.loc.row(), c = center.loc.col();
+	if(board.get(r, c-1) == ' ') { //left
+	    neighbors[0] = new Node(new Location(r, c-1), center, -1, -1, false);
+	    howMany++;
+	}
+	return neighbors;
     }
 }
