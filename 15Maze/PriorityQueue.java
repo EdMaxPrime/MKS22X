d9d914757885675a79a44c6bc442f29c9ca03416
Max Z <max.zlotskiy@gmail.com>
2017-05-19 22:07:06 -0400

    started priorityQ

--- 15Maze/PriorityQueue.java
+++ 15Maze/PriorityQueue.java
@@ -1,3 +1,19 @@
-public class PriorityQueue {
-    
+public class PriorityQueue implements Frontier {
+    MyHeap heap;
+
+    public PriorityQueue() {
+	heap = new MyHeap(false);
+    }
+
+    public void add(Node n) {
+	heap.add(n);
+    }
+
+    public boolean hasNext() {
+	return heap.size() != 0;
+    }
+
+    public Node next() {
+	return heap.remove();
+    }
 }
